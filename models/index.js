a1f8d880553f6d8bf882634d8d8f7b57d1274716
Đặng Huy Diệu <danghuydieu1243@gmail.com>
2024-11-28 20:51:09 +0700

    Add entity cart,item

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const Size = require('./size');
 const Product = require('./product');
 const User = require("./user");
 const Favorite = require("./favorite");
+const Cart = require('./cart');
 
 // Associations
 Product.belongsToMany(Size, { through: 'product_size', timestamps: false });
@@ -11,9 +12,14 @@ Size.belongsToMany(Product, { through: 'product_size', timestamps: false });
 
 Category.hasMany(Product);
 Product.belongsTo(Category);
+
 User.hasOne(Favorite);
 Favorite.belongsTo(User);
 Favorite.belongsToMany(Product, {through: 'favorite_product', timestamps: false});
 Product.belongsToMany(Favorite, {through: 'favorite_product', timestamps: false});
 
-module.exports = { sequelize, Category, Size, Product, User, Favorite };
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+
+module.exports = { sequelize, Category, Size, Product, User, Favorite, Cart };
