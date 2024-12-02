567157c5b8ea5cf7fe40bca67894b5f66631ce81
Đặng Huy Diệu <danghuydieu1243@gmail.com>
2024-12-02 18:11:10 +0700

    Add CartRoutes

--- routes/sizeRoutes.js
+++ routes/sizeRoutes.js
@@ -4,5 +4,13 @@ const sizeController = require("../controllers/sizeController");
 const router = express.Router();
 
 router.post("/sizes", sizeController.createSize);
+// Route để lấy tất cả các kích cỡ
+router.get("/sizes", sizeController.getSizes);
+
+// Route để lấy một kích cỡ theo id
+router.get("/sizes/:sizeId", sizeController.getSizeById);
+
+// Route để xóa một kích cỡ theo id
+router.delete("/sizes/:sizeId", sizeController.deleteSize);
 
 module.exports = router;
